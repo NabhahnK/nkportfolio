5f829ae8275a981b92e73b421a3ee0b91c8736c0
Nabhahn Kamaal <NabhahnK@gmail.com>
2022-09-27 12:35:39 -0700

    Changed all Home mentions to portfolio

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -17,13 +17,13 @@ function NavTabs({ currentPage, handlePageChange }) {
       </li>
       <li className="nav-item">
         <a
-          href="#home"
-          onClick={() => handlePageChange('Home')}
+          href="#portfolio"
+          onClick={() => handlePageChange('Portfolio')}
           // This is a conditional (ternary) operator that checks to see if the current page is "Home"
           // If it is, we set the current page to 'nav-link-active', otherwise we set it to 'nav-link'
-          className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
+          className={currentPage === 'Portfolio' ? 'nav-link active' : 'nav-link'}
         >
-          Home
+          Portfolio
         </a>
       </li>
       <li className="nav-item">
