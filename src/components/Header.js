ccafcb82f0ebe42afa7d1bf6f6c45c1f772111e7
Nabhahn Kamaal <NabhahnK@gmail.com>
2022-09-27 12:23:52 -0700

    added my name to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React from "react";
+import NavTabs from './NavTabs';
 
-export default function Header() {
-    return(
-        <h1>Nabhahn Kamaal</h1>
+export default function Header({ currentPage, handlePageChange }) {
+    return (
+        <header className="d-flex justify-content-between">
+            <h1>Nabhahn Kamaal</h1>
+            <NavTabs currentPage={currentPage} handlePageChange={handlePageChange} />
+        </header>
     );
 }
