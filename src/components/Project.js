a62e28fde42eeff0508d2049b1af75020f8ea240
Nabhahn Kamaal <NabhahnK@gmail.com>
2022-09-27 13:14:30 -0700

    updated project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
-export default function Header({ currentPage, handlePageChange }) {
+const style = {
+    width: '18rem'
+}
+
+export default function Project(props) {
+    const data = props;
+    const projects = data.map((data) => {
+        return(<div key={data.title} class="card" style={style.width}>
+            <img class="card-img-top" src={data.img} alt={data.imgAlt}/>
+                <div class="card-body">
+                    <h5 class="card-title">{data.title}</h5>
+                    <p class="card-text">{data.desc}</p>
+                </div>
+                <div class="card-body">
+                    <a href={data.deployed} class="card-link">{data.title}</a>
+                    <a href={data.repo} class="card-link">Github</a>
+                </div>
+        </div>)
+    })
     return (
-        <header className="d-flex justify-content-between">
-            <h1>Nabhahn Kamaal</h1>
-        </header>
+        <div>
+            {projects}
+        </div>
     );
 }
