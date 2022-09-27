330ce006c8d5dc303aee58cf9c7c6a848edc3ec1
Nabhahn Kamaal <NabhahnK@gmail.com>
2022-09-27 12:25:29 -0700

    switched about and home in the nav

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -7,6 +7,16 @@ function NavTabs({ currentPage, handlePageChange }) {
     <ul className="nav nav-tabs d-flex justify-content-end">
       <li className="nav-item">
         <a
+          href="#about"
+          onClick={() => handlePageChange('About')}
+          // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
+          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
+        >
+          About
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
           href="#home"
           onClick={() => handlePageChange('Home')}
           // This is a conditional (ternary) operator that checks to see if the current page is "Home"
@@ -14,16 +24,6 @@ function NavTabs({ currentPage, handlePageChange }) {
           className={currentPage === 'Home' ? 'nav-link active' : 'nav-link'}
         >
           Home
-        </a>
-      </li>
-      <li className="nav-item">
-        <a
-          href="#about"
-          onClick={() => handlePageChange('About')}
-          // Check to see if the currentPage is `About`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
-          className={currentPage === 'About' ? 'nav-link active' : 'nav-link'}
-        >
-          About
         </a>
       </li>
       <li className="nav-item">
